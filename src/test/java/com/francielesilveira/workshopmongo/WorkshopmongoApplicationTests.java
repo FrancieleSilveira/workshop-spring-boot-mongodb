1270fb16deefe336293836c89153f3f504ff4b5b
Franciele Silveira <franciele.silveira1603@gmail.com>
2022-08-31 23:01:42 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/francielesilveira/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francielesilveira.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
