7419a11610a86b81e7330744146e563e010df1e4
Franciele Silveira <franciele.silveira1603@gmail.com>
2022-09-01 16:04:36 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/francielesilveira/workshopmongo/domain/Post.java
+++ src/main/java/com/francielesilveira/workshopmongo/domain/Post.java
@@ -1,5 +1,6 @@
 package com.francielesilveira.workshopmongo.domain;
 
+import com.francielesilveira.workshopmongo.dto.AuthorDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -17,12 +18,12 @@ public class Post implements Serializable {
     private String title;
     private String body;
 
-    private User author;
+    private AuthorDTO author;
 
     public Post() {
     }
 
-    public Post(String id, Date date, String title, String body, User author) {
+    public Post(String id, Date date, String title, String body, AuthorDTO author) {
         super();
         this.id = id;
         this.date = date;
@@ -63,11 +64,11 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
+    public AuthorDTO getAuthor() {
         return author;
     }
 
-    public void setAuthor(User author) {
+    public void setAuthor(AuthorDTO author) {
         this.author = author;
     }
 
