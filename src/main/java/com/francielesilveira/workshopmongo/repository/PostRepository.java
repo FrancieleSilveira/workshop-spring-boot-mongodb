9e34cb683ec6070a477e9903ffd3e0172daaafb3
Franciele Silveira <franciele.silveira1603@gmail.com>
2022-09-01 15:52:18 -0300

    Criando entity post com user aninhado

---
+++ src/main/java/com/francielesilveira/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.francielesilveira.workshopmongo.repository;
+
+import com.francielesilveira.workshopmongo.domain.Post;
+import com.francielesilveira.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
