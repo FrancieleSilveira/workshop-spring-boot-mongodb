746ba6bf68d566b59e0fc16c43eddeb97d26b8f3
Franciele Silveira <franciele.silveira1603@gmail.com>
2022-09-01 11:41:05 -0300

    Obtendo um usuario por id

--- src/main/java/com/francielesilveira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/francielesilveira/workshopmongo/resources/UserResource.java
@@ -5,10 +5,7 @@ import com.francielesilveira.workshopmongo.dto.UserDTO;
 import com.francielesilveira.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,5 +25,12 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    //@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
