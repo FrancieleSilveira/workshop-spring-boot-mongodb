e9ed73a3024152a9e697dfda293fce2426d5c35d
Franciele Silveira <franciele.silveira1603@gmail.com>
2022-09-01 17:41:16 -0300

    Acrescentando comentarios aos posts

--- src/main/java/com/francielesilveira/workshopmongo/config/Instantiation.java
+++ src/main/java/com/francielesilveira/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.francielesilveira.workshopmongo.config;
 import com.francielesilveira.workshopmongo.domain.Post;
 import com.francielesilveira.workshopmongo.domain.User;
 import com.francielesilveira.workshopmongo.dto.AuthorDTO;
+import com.francielesilveira.workshopmongo.dto.CommentDTO;
 import com.francielesilveira.workshopmongo.repository.PostRepository;
 import com.francielesilveira.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.SimpleTimeZone;
 import java.util.TimeZone;
@@ -40,6 +42,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem, mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/francielesilveira/workshopmongo/domain/Post.java
+++ src/main/java/com/francielesilveira/workshopmongo/domain/Post.java
@@ -1,11 +1,14 @@
 package com.francielesilveira.workshopmongo.domain;
 
 import com.francielesilveira.workshopmongo.dto.AuthorDTO;
+import com.francielesilveira.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -17,8 +20,9 @@ public class Post implements Serializable {
     private Date date;
     private String title;
     private String body;
-
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -70,6 +74,14 @@ public class Post implements Serializable {
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
