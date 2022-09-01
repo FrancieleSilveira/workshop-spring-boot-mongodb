882f0539e9c2e4627dd35f44589b603f789351ca
Franciele Silveira <franciele.silveira1603@gmail.com>
2022-09-01 14:11:00 -0300

    Insercao de usuario com POST

--- src/main/java/com/francielesilveira/workshopmongo/resources/UserResource.java
+++ src/main/java/com/francielesilveira/workshopmongo/resources/UserResource.java
@@ -6,7 +6,9 @@ import com.francielesilveira.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,5 +34,14 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/francielesilveira/workshopmongo/services/UserService.java
+++ src/main/java/com/francielesilveira/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.francielesilveira.workshopmongo.services;
 
 import com.francielesilveira.workshopmongo.domain.User;
+import com.francielesilveira.workshopmongo.dto.UserDTO;
 import com.francielesilveira.workshopmongo.repository.UserRepository;
 import com.francielesilveira.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
